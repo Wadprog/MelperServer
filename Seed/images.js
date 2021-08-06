cccfcf1d1063801347e73f015a45444f90b8bdab
Wadson Vaval <vavalson@hotmail.com>
2021-08-05 22:18:55 -0400

    j

--- Seed/images.js
+++ Seed/images.js
@@ -3,31 +3,153 @@ const avatar_2 = 'assets/images/avatar-2.jpg';
 const avatar_3 = 'assets/images/avatar-3.jpg';
 const avatar_4 = 'assets/images/avatar-4.jpg';
 const avatar_5 = 'assets/images/avatar-5.jpg';
-const baked_fries = 'assets/images/baked-fries.jpg';
-const burger_restaurant_1 = 'assets/images/burger-restaurant.jpg';
+
+const baked_fries = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212418/melperMedia/baked-fries_fodwgi.jpg',
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212418/melperMedia/baked-fries_fodwgi.jpg',
+};
+
+const burger_restaurant_1 = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212426/melperMedia/burger-restaurant_b4jm22.jpg',
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212426/melperMedia/burger-restaurant_b4jm22.jpg',
+};
 const burger_restaurant_2 = 'assets/images/burger-restaurant-2.jpg';
-const chicago_hot_dog = 'assets/images/chicago-hot-dog.jpg';
-const crispy_chicken_burger = 'assets/images/crispy-chicken-burger.jpg';
+const chicago_hot_dog = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212412/melperMedia/chicago-hot-dog_jcyg7d.jpg',
+
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212412/melperMedia/chicago-hot-dog_jcyg7d.jpg',
+};
+
+const crispy_chicken_burger = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212441/melperMedia/crispy-chicken-burger_v94dyq.png',
+
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212441/melperMedia/crispy-chicken-burger_v94dyq.png',
+};
 const fries_restaurant = 'assets/images/fries-restaurant.jpg';
-const hawaiian_pizza = 'assets/images/hawaiian-pizza.jpg';
-const honey_mustard_chicken_burger =
-  'assets/images/honey-mustard-chicken-burger.jpg';
-const hot_dog_restaurant = 'assets/images/hot-dog-restaurant.jpg';
-const ice_kacang = 'assets/images/ice-kacang.jpg';
-const japanese_restaurant = 'assets/images/japanese-restaurant.jpg';
-const kek_lapis_shop = 'assets/images/kek-lapis-shop.jpg';
-const kek_lapis = 'assets/images/kek-lapis.jpg';
-const kolo_mee = 'assets/images/kolo-mee.jpg';
-const nasi_briyani_mutton = 'assets/images/nasi-briyani-mutton.jpg';
-const nasi_lemak = 'assets/images/nasi-lemak.jpg';
-const noodle_shop = 'assets/images/noodle-shop.jpg';
-const pizza_restaurant = 'assets/images/pizza-restaurant.jpg';
-const pizza = 'assets/images/pizza.jpg';
-const salad = 'assets/images/salad.jpg';
-const sarawak_laksa = 'assets/images/sarawak-laksa.jpg';
-const sushi = 'assets/images/sushi.jpg';
-const teh_c_peng = 'assets/images/teh-c-peng.jpg';
-const tomato_pasta = 'assets/images/tomato-pasta.jpg';
+const hawaiian_pizza = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212420/melperMedia/hawaiian-pizza_apbbki.jpg',
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212420/melperMedia/hawaiian-pizza_apbbki.jpg',
+};
+const honey_mustard_chicken_burger = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212415/melperMedia/honey-mustard-chicken-burger_rfj0pa.jpg',
+
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212415/melperMedia/honey-mustard-chicken-burger_rfj0pa.jpg',
+};
+const hot_dog_restaurant = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212429/melperMedia/hot-dog-restaurant_v9ldx2.jpg',
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212429/melperMedia/hot-dog-restaurant_v9ldx2.jpg',
+};
+const ice_kacang = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212434/melperMedia/ice-kacang_xvo42q.jpg',
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212434/melperMedia/ice-kacang_xvo42q.jpg',
+};
+
+const japanese_restaurant = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212434/melperMedia/japanese-restaurant_g7r4f5.jpg',
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212434/melperMedia/japanese-restaurant_g7r4f5.jpg',
+};
+
+const kek_lapis_shop = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212423/melperMedia/kek-lapis-shop_pvtwma.jpg',
+
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212423/melperMedia/kek-lapis-shop_pvtwma.jpg',
+};
+const kek_lapis = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212429/melperMedia/kek-lapis_si72hp.jpg',
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212429/melperMedia/kek-lapis_si72hp.jpg',
+};
+const kolo_mee = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212444/melperMedia/kolo-mee_vny8to.jpg',
+
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212444/melperMedia/kolo-mee_vny8to.jpg',
+};
+const nasi_briyani_mutton = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212422/melperMedia/nasi-briyani-mutton_l2aefn.jpg',
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212422/melperMedia/nasi-briyani-mutton_l2aefn.jpg',
+};
+const nasi_lemak = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212466/melperMedia/nasi-lemak_kczflg.jpg',
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212466/melperMedia/nasi-lemak_kczflg.jpg',
+};
+const noodle_shop = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212426/melperMedia/noodle-shop_zffrvr.jpg',
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212426/melperMedia/noodle-shop_zffrvr.jpg',
+};
+const pizza_restaurant = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212423/melperMedia/pizza-restaurant_n8dztl.jpg',
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212423/melperMedia/pizza-restaurant_n8dztl.jpg',
+};
+const pizza = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212433/melperMedia/pizza_l6gslx.jpg',
+
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212433/melperMedia/pizza_l6gslx.jpg',
+};
+const salad = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212431/melperMedia/salad_tjbmcm.jpg',
+
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212431/melperMedia/salad_tjbmcm.jpg',
+};
+const sarawak_laksa = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212430/melperMedia/sarawak-laksa_rotpmx.jpg',
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212430/melperMedia/sarawak-laksa_rotpmx.jpg',
+};
+const sushi = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212438/melperMedia/sushi_hbkl0t.jpg',
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212438/melperMedia/sushi_hbkl0t.jpg',
+};
+const teh_c_peng = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212417/melperMedia/teh-c-peng_zgohz5.jpg',
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212417/melperMedia/teh-c-peng_zgohz5.jpg',
+};
+const tomato_pasta = {
+  original:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/v1628212436/melperMedia/tomato-pasta_rlgl0y.jpg',
+  thumbnail:
+    'https://res.cloudinary.com/dnesmf7ah/image/upload/c_thumb,w_200,g_face/v1628212436/melperMedia/tomato-pasta_rlgl0y.jpg',
+};
+
 const sofa = 'assets/images/sofa.png';
 const redChair = 'assets/images/red_chair.png';
 const greenChair = 'assets/images/green_chair.png';
--- server.js
+++ server.js
@@ -6,14 +6,17 @@
 const express = require('express');
 const cors = require('cors');
 const methodOverride = require('method-override');
-
+const Stripe = require('stripe');
 // Custom dependencies
 const environnement = require('./config');
 const connectDB = require('./database');
 const isLoggedIn = require('./middleware/auth');
 const Logger = require('./utils');
 const Errors = require('./errors/errors');
-
+const PUBLISHABLE_KEY =
+  'pk_test_51HjZanEq1iLveRUg8fr4BhTbCj19fkcHL5h2TklfiExCycLF6wuPGrHOp6w1MzcMREZ50l68Rg1hHc5rZ1cWkInp005NszYz0y';
+const SECRET_KEY =
+  'sk_test_51HjZanEq1iLveRUgulSEAVEibvgmTrhciJfhPDnsK4fV6YGoUjuBP3NMuwLDTGmaP8XRtMweO9SMJPR680rrVWYH00wdVEBybx';
 // Configuring the server
 const app = express();
 // Allowing all origin
@@ -36,6 +39,8 @@ app.use((req, res, next) => {
   next();
 });
 
+const stripe = Stripe(SECRET_KEY, { apiVersion: '2020-08-27' });
+
 // Serving the routes
 app.use('/auth', require('./routes/auth'));
 app.use('/user', require('./routes/user'));
@@ -44,6 +49,25 @@ app.use('/user', require('./routes/user'));
 app.use('/store', require('./routes/store'));
 app.use('/market', require('./routes/market'));
 app.use('/payments', require('./routes/payments'));
+
+app.post('/create-payment-intent', async (req, res) => {
+  console.log('trying payment');
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1099,
+      currency: 'usd',
+      payment_method_types: ['card'],
+    });
+
+    const clientSecret = paymentIntent.client_secret;
+
+    res.json({ clientSecret });
+  } catch (e) {
+    console.log(e.message);
+    res.json({ error: e.message });
+  }
+});
+
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, _next) => {
   const { status = 500, message = Errors.unHandledServerError, ...rest } = err;
